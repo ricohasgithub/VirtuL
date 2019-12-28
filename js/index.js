ec6e21f7ca825f3f430ae7b2c0c2e903aa28fc7e
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2019-12-28 10:20:47 -0500

    Added GLTFLoader.js
     - Added a new javascript file that will load .gltf files into the browser

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
+
+// Start node.js http server: http-server . -p 8000
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor( 0xffffff );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x4287f5 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+// Function that will be used to draw WebGL component on canvas
+function animate() {
+	requestAnimationFrame( animate );
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+
+// Call the animate function to begin the drawing
+animate();
